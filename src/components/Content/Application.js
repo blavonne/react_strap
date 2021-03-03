1d1ab765268dfaf0b47a76c2f6bc39d2c7f9f3cd
blavonne <luchik_sonza@mail.ru>
2021-03-03 20:17:25 +0300

    added spinner

--- src/components/Content/Application.js
+++ src/components/Content/Application.js
@@ -50,19 +50,19 @@ class Application extends React.Component {
 		if (this.application === null)
 			return <Redirect to="/404"/>
 		else
-		return (
-			<div className="AppInfo">
-				<div>
-					<h1>Это страница {this.application.name}</h1>
+			return (
+				<div className="AppInfo">
+					<div>
+						<h1>Это страница {this.application.name}</h1>
+					</div>
+					<div>
+						Id: {this.application.id}
+					</div>
+					<div>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat neque purus. Fusce elementum commodo fermentum. Nam enim nibh, tristique non condimentum quis, sagittis in est. Sed porttitor, mauris finibus condimentum auctor, ex lectus eleifend ante, at faucibus augue sapien vel velit. Vivamus non ullamcorper tellus. Nunc fringilla velit et augue dapibus dictum. Suspendisse vestibulum lobortis posuere. Integer malesuada odio non malesuada faucibus. Nam ornare mattis nunc, non dictum augue. Sed volutpat imperdiet iaculis. Aliquam ultrices maximus nisi eu malesuada. Vestibulum ullamcorper orci vel augue varius scelerisque. Duis condimentum erat sed diam efficitur dictum. Sed vestibulum neque eget tempor ultrices.
+					</div>
 				</div>
-				<div>
-					Id: {this.application.id}
-				</div>
-				<div>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat neque purus. Fusce elementum commodo fermentum. Nam enim nibh, tristique non condimentum quis, sagittis in est. Sed porttitor, mauris finibus condimentum auctor, ex lectus eleifend ante, at faucibus augue sapien vel velit. Vivamus non ullamcorper tellus. Nunc fringilla velit et augue dapibus dictum. Suspendisse vestibulum lobortis posuere. Integer malesuada odio non malesuada faucibus. Nam ornare mattis nunc, non dictum augue. Sed volutpat imperdiet iaculis. Aliquam ultrices maximus nisi eu malesuada. Vestibulum ullamcorper orci vel augue varius scelerisque. Duis condimentum erat sed diam efficitur dictum. Sed vestibulum neque eget tempor ultrices.
-				</div>
-			</div>
-		)
+			)
 	}
 }
 
